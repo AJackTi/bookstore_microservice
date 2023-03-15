41c5ccbfbc2609b6c8d3833955aecbeb8cc04fbb
AJackTi <dtrong97vn@gmail.com>
2023-03-15 14:00:15 +0700

    feat: update create user_controller

--- bookstore_users-api/controllers/users/users_controller.go
+++ bookstore_users-api/controllers/users/users_controller.go
@@ -3,11 +3,26 @@ package users
 import (
 	"net/http"
 
+	"github.com/AJackTi/bookstore_users-api/domain/users"
+	"github.com/AJackTi/bookstore_users-api/services"
+	"github.com/AJackTi/bookstore_users-api/utils/errors"
 	"github.com/gin-gonic/gin"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "implement me!")
+	var user users.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, errors.NewBadRequestError("invalid json body"))
+		return
+	}
+
+	result, restErr := services.CreateUser(&user)
+	if restErr != nil {
+		c.JSON(restErr.Status, *restErr)
+		return
+	}
+
+	c.JSON(http.StatusCreated, *result)
 }
 
 func GetUser(c *gin.Context) {
--- bookstore_users-api/services/users_service.go
+++ bookstore_users-api/services/users_service.go
@@ -1,7 +1,12 @@
 package services
 
-func CreateUser() {
+import (
+	"github.com/AJackTi/bookstore_users-api/domain/users"
+	"github.com/AJackTi/bookstore_users-api/utils/errors"
+)
 
+func CreateUser(user *users.User) (*users.User, *errors.RestErr) {
+	return user, nil
 }
 
 func GetUser() {

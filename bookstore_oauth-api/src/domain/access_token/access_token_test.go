9511c75f61eba23fc557fa816833e7271e872593
AJackTi <dtrong97vn@gmail.com>
2023-03-20 20:38:53 +0700

    feat: init project bookstore_oauth-app, add access_token

---
+++ bookstore_oauth-api/src/domain/access_token/access_token_test.go
@@ -0,0 +1,27 @@
+package access_token
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAccessTokenConstants(t *testing.T) {
+	assert.EqualValues(t, 24, expirationTime, "expiration time should be 24 hours")
+}
+
+func TestGetAccessToken(t *testing.T) {
+	at := GetNewAccessToken()
+	assert.False(t, at.IsExpired(), "brand new access token should not be expired")
+	assert.EqualValues(t, "", at.AccessToken, "new access token should not have defined access token id")
+	assert.True(t, at.UserID == 0, "new access token should not have an associated user id")
+}
+
+func TestAccessTokenIsExpired(t *testing.T) {
+	at := AccessToken{}
+	assert.True(t, at.IsExpired(), "empty access token should be expired by default")
+	at.Expires = time.Now().UTC().Add(3 * time.Hour).Unix()
+	assert.False(t, at.IsExpired(), "access token expiring three hours from now should NOT be expired")
+
+}
